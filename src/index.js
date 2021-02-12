35b9074b41d7e47f430d514582a370ffb14e3d59
Celeste <60772363+CelesteAeon@users.noreply.github.com>
2021-02-12 00:38:42 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const CharacterBotClient = require('./Structures/CharacterBotClient');
 const config = require('../config.json');
 
 const client = new CharacterBotClient(config);
-client.start();
+client.login();
